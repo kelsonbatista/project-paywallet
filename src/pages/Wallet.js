ec6546bedb6e6f359ec26e95b0af751575fbc287
kelsonbatista <kelsonbatista@gmail.com>
2022-02-10 09:51:40 -0300

    Requisito 2

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,8 @@
 import propTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import '../assets/styles/Wallet.css';
+import MyWalletLogo from '../assets/images/mywallet2.jpeg';
 
 class Carteira extends Component {
   render() {
@@ -10,12 +12,48 @@ class Carteira extends Component {
       <>
         <header>
           <section className="header">
-            <h1>Carteira</h1>
-            <span>{ email }</span>
+            <img src={ MyWalletLogo } alt="My Wallet Logo" className="header__logo" />
+            <div className="header__right">
+              <span>{ `Email: ${email}` }</span>
+              <span>{ `Despesa Total: ${null}` }</span>
+            </div>
+          </section>
+          <section className="control">
+            <label htmlFor="control-value">
+              Valor:
+              <input type="text" id="control-value" />
+            </label>
+            <label htmlFor="control-currency">
+              Moeda:
+              <input type="text" id="control-currency" />
+            </label>
+            <label htmlFor="control-payment">
+              Método de pagamento:
+              <input type="text" id="control-payment" />
+            </label>
+            <label htmlFor="control-tag">
+              Tag:
+              <input type="text" id="control-tag" />
+            </label>
+            <label htmlFor="control-description">
+              Descrição:
+              <input type="text" id="control-description" />
+            </label>
+            <button type="submit" id="control-button">Adicionar despesa</button>
           </section>
         </header>
         <main>
-          <h2>Body</h2>
+          <section className="table">
+            <span className="table__title">Descrição</span>
+            <span className="table__title">Tag</span>
+            <span className="table__title">Método de Pagamento</span>
+            <span className="table__title">Valor</span>
+            <span className="table__title">Moeda</span>
+            <span className="table__title">Câmbio utilizado</span>
+            <span className="table__title">Valor convertido</span>
+            <span className="table__title">Moeda de conversão</span>
+            <span className="table__title">Editar/Excluir</span>
+          </section>
         </main>
       </>
     );
