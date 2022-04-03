11f19bbe2911622a0e6bb02e335397cec0841b7b
kelsonbatista <kelsonbatista@gmail.com>
2022-04-03 14:39:00 -0300

    Project update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './store';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter basename="/project-paywallet" >
+    <BrowserRouter basename="/project-paywallet">
       <App />
     </BrowserRouter>
   </Provider>,
