54a05f103790dc353fdf1b5863684ff8b5a237af
kelsonbatista <kelsonbatista@gmail.com>
2022-04-03 14:20:14 -0300

    Updates

---
+++ precache-manifest.6d18c035dd11500f29ca7066b8ff83f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3c930145c961750e61a703b0cda52e",
+    "url": "/project-paywallet/index.html"
+  },
+  {
+    "revision": "e583005bc349a32f8011",
+    "url": "/project-paywallet/static/css/main.84f5358c.chunk.css"
+  },
+  {
+    "revision": "937aac9fb6b44a95a752",
+    "url": "/project-paywallet/static/js/2.d1419509.chunk.js"
+  },
+  {
+    "revision": "0c400288f8b20bf9f3e041a9d3ed0f7c",
+    "url": "/project-paywallet/static/js/2.d1419509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e583005bc349a32f8011",
+    "url": "/project-paywallet/static/js/main.c49110a6.chunk.js"
+  },
+  {
+    "revision": "9584646db23b6d431fa2",
+    "url": "/project-paywallet/static/js/runtime-main.dbc5f35c.js"
+  },
+  {
+    "revision": "217c0234ad593f9ef7d39830463b78b1",
+    "url": "/project-paywallet/static/media/mywallet.217c0234.jpeg"
+  },
+  {
+    "revision": "79df28b65abdfc122c280e9dd2090eeb",
+    "url": "/project-paywallet/static/media/mywallet2.79df28b6.jpeg"
+  }
+]);
